6a05da4886ed71148bb5d00d9f95f765d28faefc
WesFree17 <118205059+WesFree17@users.noreply.github.com>
2023-01-17 14:52:22 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <h2>Todo List</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Black Belt MERN</li>
+        <li>30 websites in 30 days</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
